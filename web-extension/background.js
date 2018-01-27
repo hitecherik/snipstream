863e0e46ac397c4ca6f44ee4843747825a7e590f
Zak Cutner <me@zakcutner.uk>
2018-01-27 18:26:31 +0000

    Implemented skip function

--- web-extension/background.js
+++ web-extension/background.js
@@ -4,8 +4,8 @@ var BASE_URL = 'https://www.youtube.com/watch?v=';
 
 var RESPONSE = [
   { start: 1.1, end: 2.6 },
-  { start: 7.2, end: 8.8 },
-  { start: 10.3, end: 11.5 }
+  { start: 100.2, end: 105.8 },
+  { start: 200.3, end: 206.5 }
 ];
 
 chrome.runtime.onInstalled.addListener(function() {
@@ -24,5 +24,6 @@ chrome.runtime.onInstalled.addListener(function() {
 
   chrome.pageAction.onClicked.addListener(function(tab) {
     var videoId = tab.url.replace(BASE_URL, '');
+    chrome.tabs.sendMessage(tab.id, { response: RESPONSE }, function() {});
   });
 });
---
+++ web-extension/content.js
@@ -0,0 +1,40 @@
+function Loop(array) {
+  this.array = array;
+  this.index;
+  this.func;
+
+  this.forEach = function(func) {
+    this.index = -1;
+    this.func = func;
+    this.next();
+  };
+
+  this.next = function() {
+    this.index++;
+
+    if (this.index < this.array.length) {
+      this.func(this.array[this.index]);
+    }
+  };
+};
+
+chrome.runtime.onMessage.addListener(function(data, a, b) {
+  var video = document.getElementsByTagName('video')[0];
+  var loop = new Loop(data.response);
+
+  loop.forEach(function(pair) {
+    var recurse = function() {
+      if (video.currentTime > pair.end) {
+        loop.next();
+      } else {
+        setTimeout(function() {
+          recurse();
+        }, 100);
+      };
+    };
+
+    video.currentTime = pair.start;
+    recurse();
+  });
+});
+
