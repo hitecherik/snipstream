53b5c8b9e9d7e75718df877cee0eb34a5f298692
Alexander Nielsen <hitecherik@gmail.com>
2018-01-28 09:05:19 +0000

    Updated parseXML to use sentences.

--- parseXML.js
+++ parseXML.js
@@ -1,15 +1,16 @@
-const parseString = require("xml2js").parseString,
-      fs          = require("fs");
+const parseString          = require("xml2js").parseString,
+      fs                   = require("fs"),
+      SENTENCE_TERMINATORS = /[.;\?\!]/;
 
-class TimeStamp {
-  constructor(obj) {
-    this.text     = obj._.replace("\n", " ").toLowerCase();
-    this.start    = parseFloat(obj.$.start);
-    this.duration = parseFloat(obj.$.dur);
+class Sentence {
+  constructor(phrases, content) {
+    this.start   = phrases[0].$.start;
+    this.end     = phrases[phrases.length-1].$.start + phrases[phrases.length-1].$.dur;
+    this.content = content.toLowerCase();
   }
 
-  get end() {
-    return this.start + this.duration;
+  get duration() {
+    return this.end - this.start;
   }
 
   keywordInText(keyword) {
@@ -20,8 +21,41 @@ class TimeStamp {
 function parseXML(xml, callback) {
   parseString(xml, (err, result) => {
     try {
-      callback(result.transcript.text.filter(t => typeof t._ !== "undefined").map(t => new TimeStamp(t)));
+      let phrases = result.transcript.text.filter(t => typeof t._ !== "undefined");
+      let phraseAccumulator = [];
+      let sentences = [];
+      let textAccumulator = "";
+
+      for (let i = 0; i < phrases.length; i++) {
+        let subSentences = phrases[i]._.split(SENTENCE_TERMINATORS);
+
+        switch(subSentences.length) {
+          case 1:
+            phraseAccumulator.push(phrases[i]);
+            textAccumulator += phrases[i]._.replace("\n", " ") + " ";
+            break;
+          case 2:
+            phraseAccumulator.push({_: subSentences[0], $: phrases[i].$});
+            textAccumulator += subSentences[0].replace("\n", " ");
+            sentences.push(new Sentence(phraseAccumulator, textAccumulator));
+            phraseAccumulator = [{_: subSentences[1], $: phrases[i].$}];
+            textAccumulator = subSentences[1].replace("\n", " ") + " ";
+            break;
+          default:
+            phraseAccumulator.push({_: subSentences[0], $: phrases[i].$});
+            textAccumulator += subSentences[0].replace("\n", " ");
+            sentences.push(new Sentence(phraseAccumulator, textAccumulator));
+            sentences.push(new Sentence([phrases[i]], phrases[i]._.replace("\n", " ")));
+            phraseAccumulator = [{_: subSentences[subSentences.length - 1], $: phrases[i].$}];
+            textAccumulator = subSentences[subSentences.length - 1].replace("\n", " ") + " ";
+        }
+      }
+
+      sentences.push(new Sentence(phraseAccumulator, textAccumulator));
+
+      callback(sentences);
     } catch (err) {
+      throw err;
       console.log(err.message);
     }
   });
@@ -29,10 +63,10 @@ function parseXML(xml, callback) {
 
 // Testing script
 /*
- * fs.readFile("sample.xml", (err, data) => {
- *   if (err) throw err;
- *   parseXML(data, ts => console.dir(ts));
- * });
- */
+ fs.readFile("sample.xml", (err, data) => {
+   if (err) throw err;
+   parseXML(data, ts => console.dir(ts));
+ });
+*/
 
 module.exports = parseXML;
